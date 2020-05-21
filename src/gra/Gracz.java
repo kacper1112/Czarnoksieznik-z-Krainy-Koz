020a23b0fa073d25ff7313eeaf99f5024db0424a
maticoder <matikcoder@gmail.com>
2020-05-21 21:50:48 +0200

    gracz i wojo

--- src/gra/Gracz.java
+++ src/gra/Gracz.java
@@ -1,6 +1,7 @@
 package gra;
 
 public abstract class Gracz {
+    // pola klasy Gracza
     //Ekwipunek ekwipunek;
     double maksymalnePunktyZycia;
     double obecnePunktyZycia;
@@ -11,6 +12,72 @@ public abstract class Gracz {
     int postepFabularny;
     double pieniadze;
 
+    // gettery i settery Gracza
+    public double getMaksymalnePunktyZycia() {
+        return maksymalnePunktyZycia;
+    }
+
+    public void setMaksymalnePunktyZycia(double maksymalnePunktyZycia) {
+        this.maksymalnePunktyZycia = maksymalnePunktyZycia;
+    }
+
+    public double getObecnePunktyZycia() {
+        return obecnePunktyZycia;
+    }
+
+    public void setObecnePunktyZycia(double obecnePunktyZycia) {
+        this.obecnePunktyZycia = obecnePunktyZycia;
+    }
+
+    public double getSila() {
+        return sila;
+    }
+
+    public void setSila(double sila) {
+        this.sila = sila;
+    }
+
+    public double getInteligencja() {
+        return inteligencja;
+    }
+
+    public void setInteligencja(double inteligencja) {
+        this.inteligencja = inteligencja;
+    }
+
+    public int getPoziom() {
+        return poziom;
+    }
+
+    public void setPoziom(int poziom) {
+        this.poziom = poziom;
+    }
+
+    public double getPunktyDoswiadczenia() {
+        return punktyDoswiadczenia;
+    }
+
+    public void setPunktyDoswiadczenia(double punktyDoswiadczenia) {
+        this.punktyDoswiadczenia = punktyDoswiadczenia;
+    }
+
+    public int getPostepFabularny() {
+        return postepFabularny;
+    }
+
+    public void setPostepFabularny(int postepFabularny) {
+        this.postepFabularny = postepFabularny;
+    }
+
+    public double getPieniadze() {
+        return pieniadze;
+    }
+
+    public void setPieniadze(double pieniadze) {
+        this.pieniadze = pieniadze;
+    }
+
+    // konstruktor domyslny
     public Gracz() {
         maksymalnePunktyZycia = 100;
         obecnePunktyZycia = maksymalnePunktyZycia;
@@ -22,6 +89,20 @@ public abstract class Gracz {
         pieniadze = 100;
     }
 
+    // konstruktor klasy gracza
+    public Gracz(double maksymalnePunktyZycia, double sila, double inteligencja) {
+        this.maksymalnePunktyZycia = maksymalnePunktyZycia;
+        this.obecnePunktyZycia = maksymalnePunktyZycia;
+        this.sila = sila;
+        this.inteligencja = inteligencja;
+        this.poziom = 1;
+        this.punktyDoswiadczenia = 0;
+        this.postepFabularny = 0;
+        this.pieniadze = 100;
+    }
+
+    // gracz otrzymuje obrazenia
+    // dodac co sie dzieje, gdy obecnePunktyZycia = 0
     void zmniejszPunktyZycia(double wartosc) {
         if(obecnePunktyZycia <= wartosc) {
             obecnePunktyZycia = 0;
@@ -30,12 +111,17 @@ public abstract class Gracz {
         }
     }
 
+    // gracz zwieksza swoje punkty zycia
     void zwiekszPunktyZycia(double wartosc) {
-       if(obecnePunktyZycia + wartosc >= maksymalnePunktyZycia) {
-           obecnePunktyZycia = maksymalnePunktyZycia;
-       } else {
-           obecnePunktyZycia += wartosc;
-       }
+        if(obecnePunktyZycia + wartosc >= maksymalnePunktyZycia) {
+            obecnePunktyZycia = maksymalnePunktyZycia;
+        } else {
+            obecnePunktyZycia += wartosc;
+        }
+    }
+
+    double zadajObrazenia() {
+
     }
 
     double obliczWplywAtrybutowPrzedmiotowNaOtrzymanePrzezWojownikaObrazenia() {return 0;}
@@ -51,11 +137,4 @@ public abstract class Gracz {
 //    void uzyjPrzedmiotu(Fabularny fabularny) {
 //
 //    }
-
 }
-
-
-
-
-
-
--- src/gra/Wojownik.java
+++ src/gra/Wojownik.java
@@ -1,10 +1,13 @@
 package gra;
 
 public class Wojownik extends Gracz {
+    public Wojownik() {
+        super(120, 20, 10);
+    }
+
+    // obsluzyc jak gracza ginie
     void otrzymajObrazenia(double wartosc) {
         // obrazenia zostaja pomniejszone o tyle % ile sily ma wojownik
-
-
 
         double obrazenia = (1 - sila / 100) * wartosc;
         zmniejszPunktyZycia(obrazenia);
