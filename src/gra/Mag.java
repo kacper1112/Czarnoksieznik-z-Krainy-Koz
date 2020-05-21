d2cbce6d81ad318b77854e26fc7e8d4a1e693b75
maticoder <matikcoder@gmail.com>
2020-05-21 23:51:32 +0200

    gracze

--- src/gra/Mag.java
+++ src/gra/Mag.java
@@ -1,7 +1,30 @@
 package gra;
 
 public class Mag extends Gracz {
-    void otrzymajObrazenia(double wartosc) {
+    public Mag() {
+        super(100, 10, 20);
+    }
 
+    // dodac w zaleznosci od przedmiotu i atrybutow
+    public double zadajObrazenia() {
+        return this.getInteligencja() + this.ekwipunek.getWyekwipowanaBron().wartosc;
+    }
+
+    // obsluzyc jak gracza ginie
+    public void otrzymajObrazenia(double wartosc) {
+        // mozwliwosc uniku
+        if(Math.random() >= .1) {
+            // obrazenia zostaja pomniejszone o tyle % ile sily ma wojownik
+            double obrazenia = (1 - inteligencja / 100) * wartosc;
+            zmniejszPunktyZycia(obrazenia);
+        }
+    }
+
+    public void zwiekszLevel() {
+        if(this.getPunktyDoswiadczenia() > 100) {
+            this.setPoziom(this.getPoziom() + 1);
+            this.setPunktyDoswiadczenia(this.getPunktyDoswiadczenia() % 100);
+            this.setInteligencja(this.getInteligencja() + 10);
+        }
     }
 }
