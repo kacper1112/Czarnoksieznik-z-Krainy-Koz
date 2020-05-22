2aa8f08bbb878040f65abe530aa9289b208bb8de
kacper1112 <kaacper1112@gmail.com>
2020-05-22 21:21:09 +0200

    naprawiony konstruktor PrzedmiotPozywienie

--- src/gra/PrzedmiotPozywienie.java
+++ src/gra/PrzedmiotPozywienie.java
@@ -3,7 +3,8 @@ package gra;
 public class PrzedmiotPozywienie extends Przedmiot {
     double przywracaneZycie;
 
-    public PrzedmiotPozywienie(double przywracaneZycie) {
+    public PrzedmiotPozywienie(String nazwa, String opis, double wartosc, int szansaAtrybutu, double przywracaneZycie) {
+        super(nazwa, opis, wartosc, szansaAtrybutu);
         this.przywracaneZycie = przywracaneZycie;
     }
 
