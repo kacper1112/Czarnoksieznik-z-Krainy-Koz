f5c6ec18c196a5a1d6a5ff67c860764aa6b77853
Kacper Polak <kacperp10@interia.pl>
2020-05-22 15:13:23 +0200

    szosty

--- src/gra/Handlarz.java
+++ src/gra/Handlarz.java
@@ -1,5 +1,6 @@
 package gra;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -12,19 +13,80 @@ public class Handlarz extends NPC {
         Random random = new Random();
         int bonus =  random.nextInt(100);
         pieniadze = 300 + bonus;
+        oferta = new ArrayList<>();
     }
 
     @Override
     public Ekwipunek generujEkwipunek() {
-        return null;
+        Ekwipunek ekwipunek = new Ekwipunek();
+        List<Para<String, String> > pozywienieTMP = List.of(
+                new Para<>("Mikstura Lowcy", "opis"),
+                new Para<>("Elikis Gniewu", "opis"),
+                new Para<>("Eliksir Zdrowia", "opis"),
+                new Para<>("Jablko", "opis"),
+                new Para<>("Chleb", "opis"),
+                new Para<>("Mieso", "opis")
+                );
+        List<Para<String, String>> bronFizycznaTMP = List.of(
+                new Para<>("Rteciowy Bula", "opis"),
+                new Para<>("Wlucznia Shojin", "opis"),
+                new Para<>("Plonoce Ostrze", "opis"),
+                new Para<>("Czarny Tasak", "opis")
+        );
+        List<Para<String, String>> bronMagicznaTMP = List.of(
+                new Para<>("Rozdzka Wiekow", "opis"),
+                new Para<>("Kostur Pustki", "opis"),
+                new Para<>("Echo Luden", "opis"),
+                new Para<>("Klinga Burzy", "opis")
+        );
+
+        Random rand = new Random();
+
+        int pairIndex = rand.nextInt(bronFizycznaTMP.size());
+        ekwipunek.wlozBron(new BronFizyczna(
+                bronFizycznaTMP.get(pairIndex).getPierwszy(),
+                bronFizycznaTMP.get(pairIndex).getDrugi(),
+                rand.nextDouble()*100,
+                rand.nextInt(100),
+                rand.nextDouble()*100,
+                rand.nextDouble()*100
+        ));
+
+        pairIndex = rand.nextInt(bronFizycznaTMP.size());
+        ekwipunek.wlozBron( new BronMagiczna(
+                bronMagicznaTMP.get(pairIndex).getPierwszy(),
+                bronMagicznaTMP.get(pairIndex).getDrugi(),
+                rand.nextDouble()*100,
+                rand.nextInt(100),
+                rand.nextDouble()*100,
+                rand.nextDouble()*100
+        ));
+
+        /* JAK BEDZIE ZROBIONE POZYWIENIE
+                pairIndex = rand.nextInt(pozywienieTMP.size());
+        ekwipunek.wlozPozywienie(new Pozywienie(
+                pozywienieTMP.get(pairIndex).getPierwszy(),
+                pozywienieTMP.get(pairIndex).getDrugi()
+        ));
+         */
+
+        ekwipunek.getEkwipunekBron().forEach(x->{
+            oferta.add((Przedmiot) x);
+        });
+
+        ekwipunek.getEkwipunekPozywienie().forEach(x->{
+            oferta.add((Przedmiot) x);
+        });
+
+        return ekwipunek;
     }
 
     public void przedstawOferte(){
-        System.out.println("Moja oferta dla ciebie byczq: " + this.oferta);
+        System.out.println("Moja oferta dla ciebie byczq: \n" + this.oferta);
     }
 
     public void resetujOferte(){
-
+        setEkwipunek(generujEkwipunek());
     }
 
     void sprzedajGraczowi(Gracz g){
---
+++ src/gra/Para.java
@@ -0,0 +1,43 @@
+package gra;
+
+import java.util.Objects;
+
+public class Para <A, B> {
+    private A pierwszy;
+    private B drugi;
+
+    public Para(A pierwszy, B second) {
+        this.pierwszy = pierwszy;
+        this.drugi = second;
+    }
+
+    public A getPierwszy() {
+        return pierwszy;
+    }
+
+    public void setPierwszy(A pierwszy) {
+        this.pierwszy = pierwszy;
+    }
+
+    public B getDrugi() {
+        return drugi;
+    }
+
+    public void setDrugi(B second) {
+        this.drugi = second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Para<?, ?> para = (Para<?, ?>) o;
+        return Objects.equals(pierwszy, para.pierwszy) &&
+                Objects.equals(drugi, para.drugi);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pierwszy, drugi);
+    }
+}
