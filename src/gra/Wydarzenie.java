928e2aec7583a3502684473b3348a1bc2c65452b
kacper1112 <kaacper1112@gmail.com>
2020-05-25 11:49:07 +0200

    dodatkowe konstruktory Wydarzenie

--- src/gra/Wydarzenie.java
+++ src/gra/Wydarzenie.java
@@ -1,51 +1,51 @@
 package gra;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Wydarzenie {
     private final String nazwa;
     private final String opis;
     private final Gracz gracz;
+    private final List<Fabularny> postacieFabularne;
+    private final List<Wrog> wrogowie;
     private final Boss boss;
-    private List<Wrog> wrogowie;
-    private List<Fabularny> postacieFabularne;
 
-    public Wydarzenie(String nazwa, String opis, Gracz gracz, Boss boss) {
+    public Wydarzenie(String nazwa, String opis, Gracz gracz, List<Fabularny> fabularne, int iluWrogow, Boss boss) {
         this.nazwa = nazwa;
         this.opis = opis;
         this.gracz = gracz;
+        this.postacieFabularne = fabularne;
+        this.wrogowie = generujWrogow(iluWrogow);
         this.boss = boss;
     }
 
-    public Wydarzenie(String nazwa, String opis, Gracz gracz) {
+    public Wydarzenie(String nazwa, String opis, Gracz gracz, int iluWrogow) {
         this.nazwa = nazwa;
         this.opis = opis;
         this.gracz = gracz;
+        this.postacieFabularne = null;
+        this.wrogowie = generujWrogow(iluWrogow);
         this.boss = null;
     }
 
-    public void dodajWroga(Wrog wrog) {
-        this.wrogowie.add(wrog);
+    public Wydarzenie(String nazwa, String opis, Gracz gracz, List<Fabularny> fabularne, int iluWrogow) {
+        this.nazwa = nazwa;
+        this.opis = opis;
+        this.gracz = gracz;
+        this.postacieFabularne = fabularne;
+        this.wrogowie = generujWrogow(iluWrogow);
+        this.boss = null;
     }
 
-    public void dodajPostacFabularna(Fabularny fabularny) {
-        this.postacieFabularne.add(fabularny);
-    }
-
-    public String getNazwa() {
-        return nazwa;
-    }
-
-    public String getOpis() {
-        return opis;
-    }
-
-    public List<Wrog> getWrogowie() {
-        return wrogowie;
-    }
-
-    public List<Fabularny> getPostacieFabularne() {
-        return postacieFabularne;
+    private List<Wrog> generujWrogow(int iluWrogow) {
+        List<Wrog> tempWrogowie = new ArrayList<>();
+        for(int i = 0; i < iluWrogow; i++) {
+            Wrog wrog = new Wrog("generyczny wrog");
+            tempWrogowie.add(wrog);
+            // todo generowanie wrogow
+        }
+        return tempWrogowie;
     }
 
     private void rozpocznijWydarzenie() {
