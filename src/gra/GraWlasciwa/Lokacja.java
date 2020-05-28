dcfd4a6bec39a490c45731992787dad01cdc8c42
kacper1112 <kaacper1112@gmail.com>
2020-05-29 01:24:50 +0200

    Tablica opisow na pojedynczy opis

--- src/gra/GraWlasciwa/Lokacja.java
+++ src/gra/GraWlasciwa/Lokacja.java
@@ -4,18 +4,18 @@ import gra.GraWlasciwa.Wydarzenie;
 
 public class Lokacja {
     private final String nazwa;
-    private final String[] opisy;
+    private final String opis;
     private final int[] sasiednieLokacje;
     private final Wydarzenie wydarzenieFabularne;
     private final Wydarzenie[] wydarzeniaPoboczne;
 
     public Lokacja(String nazwa,
-                   String[] opisy,
+                   String opis,
                    Wydarzenie fabularne,
                    Wydarzenie[] poboczne,
                    int[] sasiednie) {
         this.nazwa = nazwa;
-        this.opisy = opisy;
+        this.opis = opis;
         this.wydarzenieFabularne = fabularne;
         this.wydarzeniaPoboczne = poboczne;
         this.sasiednieLokacje = sasiednie;
