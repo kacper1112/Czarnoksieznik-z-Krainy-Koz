45a3d09a5fdf73c2830a010b8294c0d883d71ed0
maticoder <matikcoder@gmail.com>
2020-06-06 00:10:55 +0200

    za weglowodanami i plackami z hita

--- src/gra/ElementyPomocnicze/Zagadka.java
+++ src/gra/ElementyPomocnicze/Zagadka.java
@@ -1,23 +1,29 @@
 package gra.ElementyPomocnicze;
 
+import gra.RodzajeGracz.Gracz;
+import gra.RodzajePrzedmiot.PrzedmiotFabularny;
+
 import java.util.Scanner;
 
 public class Zagadka {
     private final String zagadka;
     private final String odpowiedz;
+    private final PrzedmiotFabularny przedmiotFabularny;
     Scanner in = new Scanner(System.in);
 
-    public Zagadka(String zagadka, String odpowiedz) {
+    public Zagadka(String zagadka, String odpowiedz, PrzedmiotFabularny przedmiotFabularny) {
         this.zagadka = zagadka;
         this.odpowiedz = odpowiedz;
+        this.przedmiotFabularny = przedmiotFabularny;
     }
 
-    public void wywolajZagadke() {
+    public void wywolajZagadke(Gracz gracz) {
         String odpowiedzGracza = "";
         while(!odpowiedzGracza.equals(this.odpowiedz)) {
             System.out.println(this.zagadka);
             odpowiedzGracza = in.nextLine().trim();
         }
+        gracz.wlozPrzedmiotFabularnyDoEkwipunku(przedmiotFabularny);
     }
 
 
