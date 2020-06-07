c91dcc21bd5f4c64998f737f493486bc681dd158
kacper1112 <kaacper1112@gmail.com>
2020-06-08 00:28:32 +0200

    ladniejsze formatowanie

--- src/gra/ElementyPomocnicze/Ekwipunek.java
+++ src/gra/ElementyPomocnicze/Ekwipunek.java
@@ -25,13 +25,16 @@ public class Ekwipunek {
     public void wlozFabularne(PrzedmiotFabularny pf) {
         ekwipunekFabularne.add(pf);
     }
+
     public void wlozPozywienie(PrzedmiotPozywienie pozywienie) {
         ekwipunekPozywienie.add(pozywienie);
     }
-    public void wlozBronMagiczna(BronMagiczna p){
+
+    public void wlozBronMagiczna(BronMagiczna p) {
         ekwipunekBronMagiczna.add(p);
     }
-    public void wlozBronFizyczna(BronFizyczna p){
+
+    public void wlozBronFizyczna(BronFizyczna p) {
         ekwipunekBronFizczna.add(p);
     }
 
@@ -51,32 +54,33 @@ public class Ekwipunek {
         return ekwipunekBronFizczna.size();
     }
 
-    public void wyciagnijBronFizyczna(int indeks){
+    public void wyciagnijBronFizyczna(int indeks) {
         ekwipunekBronFizczna.remove(indeks);
     }
-    public void wyciagnijBronMagiczna(int indeks){
+
+    public void wyciagnijBronMagiczna(int indeks) {
         ekwipunekBronMagiczna.remove(indeks);
     }
 
-    public void wyciagnijFabularne(int indeks){
+    public void wyciagnijFabularne(int indeks) {
         ekwipunekFabularne.remove(indeks);
     }
 
-    public void wyciagnijPozywienie(int indeks){
+    public void wyciagnijPozywienie(int indeks) {
         ekwipunekPozywienie.remove(indeks);
     }
 
-    public void zmienWyekwipowanaBronNaMagiczna(int indeksBroni){
-        if(TYP == TYP_POSIADACZA_EKWIPUNKU.WOJOWNIK){
+    public void zmienWyekwipowanaBronNaMagiczna(int indeksBroni) {
+        if (TYP == TYP_POSIADACZA_EKWIPUNKU.WOJOWNIK) {
             System.out.println("Nie mozesz korzystac z broni magicznej - jestes wojownikiem");
-        }else {
+        } else {
             wyekwipowanaBron = ekwipunekBronMagiczna.get(indeksBroni);
             System.out.println("Zmieniles bron na: " + wyekwipowanaBron);
         }
     }
 
-    public void zmienWyekwipowanaBronNaFizyczna(int indeksBroni){
-        if(TYP == TYP_POSIADACZA_EKWIPUNKU.MAG) {
+    public void zmienWyekwipowanaBronNaFizyczna(int indeksBroni) {
+        if (TYP == TYP_POSIADACZA_EKWIPUNKU.MAG) {
             System.out.println("Nie mozesz korzystac z broni fizycznej - jestes magiem");
         } else {
             wyekwipowanaBron = ekwipunekBronFizczna.get(indeksBroni);
@@ -84,14 +88,12 @@ public class Ekwipunek {
         }
     }
 
-    public void setTYP(TYP_POSIADACZA_EKWIPUNKU TYP) {
-        this.TYP = TYP;
-    }
-
-    //GETERY I SETTERY (CZESC DO WYWALENIA POZNIEJ)
-
     public TYP_POSIADACZA_EKWIPUNKU getTYP() {
         return TYP;
+    }
+
+    public void setTYP(TYP_POSIADACZA_EKWIPUNKU TYP) {
+        this.TYP = TYP;
     }
 
     public List<PrzedmiotFabularny> getEkwipunekFabularne() {
@@ -121,59 +123,59 @@ public class Ekwipunek {
 
     public int pokazEkwipunek() {
         int indeks = 1;
-        if(this.isEmpty()) {
+        if (this.isEmpty()) {
             System.out.println("Twoj ekwipunek jest pusty!");
             return 0;
         }
 
-        if(TYP==TYP_POSIADACZA_EKWIPUNKU.WOJOWNIK||TYP==TYP_POSIADACZA_EKWIPUNKU.MAG||TYP==TYP_POSIADACZA_EKWIPUNKU.KAPLAN){
+        if (TYP == TYP_POSIADACZA_EKWIPUNKU.WOJOWNIK || TYP == TYP_POSIADACZA_EKWIPUNKU.MAG || TYP == TYP_POSIADACZA_EKWIPUNKU.KAPLAN) {
             System.out.println("Twoje przedmioty:");
-        }else if(TYP == TYP_POSIADACZA_EKWIPUNKU.HANDLARZ) {
-            System.out.println("Witaj przybyszu!\n"+"Oto moja oferta, spcjalnie dla ciebie:");
-        }else if(TYP == TYP_POSIADACZA_EKWIPUNKU.BOSS ||TYP == TYP_POSIADACZA_EKWIPUNKU.WROG){
+        } else if (TYP == TYP_POSIADACZA_EKWIPUNKU.HANDLARZ) {
+            System.out.println("Witaj przybyszu!\n" + "Oto moja oferta, spcjalnie dla ciebie:");
+        } else if (TYP == TYP_POSIADACZA_EKWIPUNKU.BOSS || TYP == TYP_POSIADACZA_EKWIPUNKU.WROG) {
             System.out.println("Oto ekwipunek twego przeciwnika:");
         }
 
-        if(!this.getEkwipunekPozywienie().isEmpty()) {
+        if (!this.getEkwipunekPozywienie().isEmpty()) {
             System.out.println("Pozywienie:");
-            for(PrzedmiotPozywienie p: this.getEkwipunekPozywienie()) {
+            for (PrzedmiotPozywienie p : this.getEkwipunekPozywienie()) {
                 System.out.println(indeks++ + ". " + p.getNazwa());
             }
         }
-        if(!this.getEkwipunekBronFizyczna().isEmpty()) {
+        if (!this.getEkwipunekBronFizyczna().isEmpty()) {
             System.out.println("Bron fizyczna:");
-            for(BronFizyczna bron: this.getEkwipunekBronFizyczna()) {
+            for (BronFizyczna bron : this.getEkwipunekBronFizyczna()) {
                 //System.out.println(indeks++ + ". " + bron.getNazwa());
                 System.out.println(indeks++ + ". " + bron);
             }
         }
-        if(!this.getEkwipunekBronMagiczna().isEmpty()) {
+        if (!this.getEkwipunekBronMagiczna().isEmpty()) {
             System.out.println("Bron magiczna:");
-            for(BronMagiczna bron: this.getEkwipunekBronMagiczna()) {
+            for (BronMagiczna bron : this.getEkwipunekBronMagiczna()) {
                 //System.out.println(indeks++ + ". " + bron.getNazwa());
                 System.out.println(indeks++ + ". " + bron);
             }
         }
-        if(!this.getEkwipunekFabularne().isEmpty()) {
+        if (!this.getEkwipunekFabularne().isEmpty()) {
             System.out.println("Przedmioty fabularne:");
-            for(PrzedmiotFabularny p: this.getEkwipunekFabularne()) {
+            for (PrzedmiotFabularny p : this.getEkwipunekFabularne()) {
                 System.out.println(indeks++ + ". " + p.getNazwa());
             }
         }
         return indeks - 1;
     }
 
-    public void dodajEkwipunek(Ekwipunek ekwipunekPrzeciwnika){
-        if(!ekwipunekPrzeciwnika.getEkwipunekPozywienie().isEmpty()) {
+    public void dodajEkwipunek(Ekwipunek ekwipunekPrzeciwnika) {
+        if (!ekwipunekPrzeciwnika.getEkwipunekPozywienie().isEmpty()) {
             ekwipunekPrzeciwnika.getEkwipunekPozywienie().forEach(this::wlozPozywienie);
         }
-        if(!ekwipunekPrzeciwnika.getEkwipunekBronFizyczna().isEmpty()) {
+        if (!ekwipunekPrzeciwnika.getEkwipunekBronFizyczna().isEmpty()) {
             ekwipunekPrzeciwnika.getEkwipunekBronFizyczna().forEach(this::wlozBronFizyczna);
         }
-        if(!ekwipunekPrzeciwnika.getEkwipunekBronMagiczna().isEmpty()) {
+        if (!ekwipunekPrzeciwnika.getEkwipunekBronMagiczna().isEmpty()) {
             ekwipunekPrzeciwnika.getEkwipunekBronMagiczna().forEach(this::wlozBronMagiczna);
         }
-        if(!ekwipunekPrzeciwnika.getEkwipunekFabularne().isEmpty()) {
+        if (!ekwipunekPrzeciwnika.getEkwipunekFabularne().isEmpty()) {
             ekwipunekPrzeciwnika.getEkwipunekFabularne().forEach(this::wlozFabularne);
         }
     }
