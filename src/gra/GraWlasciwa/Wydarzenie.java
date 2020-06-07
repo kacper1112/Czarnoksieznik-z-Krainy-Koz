759d7b75e3384e389c30be63de02f4a3774aeaf5
Kacper Polak <kacperp10@interia.pl>
2020-06-07 17:33:19 +0200

    jakies male poprawki

--- src/gra/GraWlasciwa/Wydarzenie.java
+++ src/gra/GraWlasciwa/Wydarzenie.java
@@ -193,61 +193,6 @@ public class Wydarzenie {
         this.zagadka.wywolajZagadke(gracz);
     }
 
-    /*
-    private boolean walka(Wrog wrog) {
-        int wyborGracza;
-        boolean walkaTrwa = true;
-        double obrazenia;
-        // true - gracz, false - wrog
-        boolean kolejGracza = (Math.random() < 0.5);
-        if(kolejGracza) {
-            System.out.println("Walke rozpoczyna Twoj przeciwnik!");
-        } else {
-            System.out.println("Rozpoczynasz walke!");
-        }
-
-        while(walkaTrwa) {
-            if(kolejGracza) {
-                System.out.println("Co chcesz zrobic?\n1.Skorzystaj z ekwipunku\n2.Zaatakuj przeciwnika" +
-                        "korzystajac z " + gracz.getEkwipunek().getWyekwipowanaBron() + "\n" +
-                        "3.Sprobuj wykonac atak specjalny");
-
-                wyborGracza = Gra.wczytajWyborGracza(3, false);
-                if(wyborGracza == 1) {
-                    Menu.menuEkwipunku();
-                    // menu wyboru przedmiotu z ekwipunku
-                } else if(wyborGracza == 2) {
-                    obrazenia = gracz.zadajObrazenia();
-                    System.out.println("Atakujesz wroga za " + obrazenia + " punktow!");
-                    wrog.otrzymajObrazenia(obrazenia);
-                    System.out.println("Wrog ma teraz " + wrog.getObecnePunktyZycia() + "/" +
-                            wrog.getMaksymalnePunktyZycia() + " punktow zycia.");
-                } else if(wyborGracza == 3) {
-                    obrazenia = gracz.zadajObrazeniaSpecjalne();
-                    System.out.println("Atakujesz wroga za " + obrazenia + " punktow!");
-                    wrog.otrzymajObrazenia(obrazenia);
-                    System.out.println("Wrog ma teraz " + wrog.getObecnePunktyZycia() + "/" +
-                            wrog.getMaksymalnePunktyZycia() + " punktow zycia.");
-                }
-
-            } else {
-                obrazenia = wrog.zadajObrazenia();
-                System.out.println(wrog.getImie() + " zadaje Ci " + obrazenia + "punktow obrazen! ");
-                gracz.otrzymajObrazenia(obrazenia);
-                System.out.println("Masz teraz " + gracz.getObecnePunktyZycia() + "/" +
-                        gracz.getMaksymalnePunktyZycia() + " punktow zycia.");
-            }
-
-            if(gracz.getObecnePunktyZycia() <= 0 || wrog.getObecnePunktyZycia() <= 0) {
-                walkaTrwa = false;
-            }
-
-            kolejGracza = !kolejGracza;
-        }
-        return !kolejGracza;
-    }
-     */
-
     public Zagadka getZagadka() {
         return zagadka;
     }
