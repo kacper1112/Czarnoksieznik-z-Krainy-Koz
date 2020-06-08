ecd3831608ce289754d2c669ae540c60a694e418
Kacper Polak <kacperp10@interia.pl>
2020-06-08 21:16:32 +0200

    Para testy

---
+++ test/gra/ElementyPomocnicze/ParaTest.java
@@ -0,0 +1,34 @@
+package gra.ElementyPomocnicze;
+
+import org.junit.Test;
+
+public class ParaTest {
+
+    @Test
+    public void getPierwszy() {
+    }
+
+    @Test
+    public void setPierwszy() {
+    }
+
+    @Test
+    public void getDrugi() {
+    }
+
+    @Test
+    public void setDrugi() {
+    }
+
+    @Test
+    public void testEquals() {
+    }
+
+    @Test
+    public void testHashCode() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+}
