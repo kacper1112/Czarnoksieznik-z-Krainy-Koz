0db1e65a261d11b7c0547c1d8ec4f8f57960d763
Kacper Polak <kacperp10@interia.pl>
2020-06-09 04:31:21 +0200

    PrzedmiotTest oraz PrzedmiotPozywienieTest oraz PrzedmiotFabularnyTest

--- test/gra/RodzajePrzedmiot/PrzedmiotFabularnyTest.java
+++ test/gra/RodzajePrzedmiot/PrzedmiotFabularnyTest.java
@@ -6,13 +6,16 @@ import static org.junit.Assert.*;
 
 public class PrzedmiotFabularnyTest {
 
-    PrzedmiotFabularny przedmiotFabularny = new PrzedmiotFabularny("pf","pfOpis",)
+    PrzedmiotFabularny przedmiotFabularny = new PrzedmiotFabularny("pf","pfOpis", 100,100,false,"aaa",100);
 
     @Test
     public void getWskazowka() {
+        assertEquals(przedmiotFabularny.getWskazowka(10000),"aaa");
     }
 
     @Test
     public void testToString() {
+        String tmp = "pf";
+        assertEquals(przedmiotFabularny.toString(),tmp);
     }
 }
